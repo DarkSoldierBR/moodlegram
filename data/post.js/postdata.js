534d8f41175261588939af48553c8f7f33c511bc
DarkSoldierBR <mateus.dias029@gmail.com>
2021-01-30 23:16:01 -0300

    feat: populate name in postData

--- data/post.js/postdata.js
+++ data/post.js/postdata.js
@@ -10,24 +10,24 @@ export const state = {
 		{
 			id: 1,
 			name: "Beatriz Orue",
-			postProfileImageSrc: require("../../src/img/profile/profile1.png"),
+			postProfileImageSrc: require("../../src/img/profile/profile4.png"),
 			postImageSrc: require("../../src/img/post/post1.png"),
 		},
 		{
 			id: 2,
-			name: "Beatriz Orue",
+			name: "Aline Faria",
 			postProfileImageSrc: require("../../src/img/profile/profile2.png"),
 			postImageSrc: require("../../src/img/post/post2.jpg"),
 		},
 		{
 			id: 3,
-			name: "Beatriz Orue",
+			name: "Carol Salgueiro",
 			postProfileImageSrc: require("../../src/img/profile/profile3.png"),
 			postImageSrc: require("../../src/img/post/post3.png"),
 		},
 		{
 			id: 4,
-			name: "Beatriz Orue",
+			name: "Alexandra Dandario",
 			postProfileImageSrc: require("../../src/img/profile/profile4.png"),
 			postImageSrc: require("../../src/img/post/post4.jpg"),
 		},
